4fc132a882dedff91fd58b8a9057b491e12a21fe
Chandni <chandni.manak@gmail.com>
2019-05-24 12:32:23 +0530

    Update deployArtifacts.groovy

--- vars/deployArtifacts.groovy
+++ vars/deployArtifacts.groovy
@@ -1,20 +1,20 @@
-def call(String artifactoryName, String artifact, String repo){
-        echo 'inside artifact script'
-        def SERVER_ID = "${artifactoryName}" 
+def call(String artifactoryServerName, String artifactDeployed, String repo){
+        echo 'Deploying artifacts to Artifactory..'
+        def SERVER_ID = "${artifactoryServerName}" 
         def server = Artifactory.server SERVER_ID
         def uploadFile = 
         """
         {
         "files": [
             {
-                "pattern": "${artifact}",
+                "pattern": "${artifactDeployed}",
                 "target": "${repo}/${BUILD_NUMBER}/"
             }
         ]
         }
         """
-        echo 'below pattern and target artifact script'
         def buildInfo = Artifactory.newBuildInfo() 
         buildInfo=server.upload(uploadFile) 
         server.publishBuildInfo(buildInfo)
+        echo "Artifacts deployed to artifactory!"
     }
