40841f22b495f549361aa5f08349fd93fa71fd3d
Chandni <chandni.manak@gmail.com>
2019-05-25 12:18:29 +0530

    Update uploadDockerImage.groovy

--- vars/uploadDockerImage.groovy
+++ vars/uploadDockerImage.groovy
@@ -1,5 +1,10 @@
 def call(String imageName){
 
-sh "docker push ${imageName}:${BUILD_NUMBER}"
+//sh "docker push ${imageName}:${BUILD_NUMBER}"
 
+ docker.withRegistry('https://hub.docker.com', 'DockerHub') {
+            app.push("${imageName}")
+            app.push("${env.BUILD_NUMBER}")
+  }
+  
 }
