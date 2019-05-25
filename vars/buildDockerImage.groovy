e3f1d329366673acebe53ce3ac6f2b3bd9716405
Chandni <chandni.manak@gmail.com>
2019-05-25 13:03:16 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,7 @@
 def call(String imageName){
 
+ docker.withRegistry('https://registry.hub.docker.com', 'DockerHub') {
     image = docker.build("${imageName}:${BUILD_NUMBER}")
-
+    image.push()
+   }
 }
